1f35708174a43487b8ce9aa02ba3258ec53edb1e
Ruslan Negoda <iarjuna108@gmail.com>
2023-02-01 16:05:20 +0200

    qweqew

--- contacts.js
+++ contacts.js
@@ -1,11 +1,18 @@
-/*
- * Розкоментуйте і запиши значення
- * const contactsPath = ;
- */
+const path = require("path");
+const fs = require("fs").promises;
+
+const contactsPath = path.resolve("./db/contacts.json");
+console.log(contactsPath);
 
 // TODO: задокументувати кожну функцію
-function listContacts() {
-  // ...твій код
+async function listContacts() {
+  try {
+    const data = await fs.readFile("contactsPath", "utf-8");
+    const contacts = JSON.parse(data);
+    return contacts;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 function getContactById(contactId) {
--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 console.log("RUSLAN --- HELLO");
 console.log(process.argv);
+
+const path = require("path");
+
+const contactsPath = path.resolve("./db/contacts.json");
+console.log(contactsPath);
