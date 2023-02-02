54945929f3911027e59bfa5d934cbffe7c66a72e
Ruslan Negoda <iarjuna108@gmail.com>
2023-02-02 14:32:54 +0200

    Update contacts.js

--- contacts.js
+++ contacts.js
@@ -3,38 +3,48 @@ const fs = require("fs").promises;
 
 const contactsPath = path.resolve("./db/contacts.json");
 
-async function readDb() {
-  try {
-    const data = await fs.readFile(contactsPath, "utf-8");
-    const readContacts = JSON.parse(data);
-    return readContacts;
-  } catch (error) {
-    console.error(error);
-  }
-}
-
 async function wrightDb(db) {
   await fs.writeFile(contactsPath, JSON.stringify(db, null, -2));
 }
 
 // TODO: задокументувати кожну функцію
 async function listContacts() {
-  const contacts = await readDb();
-  console.table(contacts);
-  return contacts;
+  try {
+    const data = await fs.readFile(contactsPath, "utf-8");
+    const contacts = JSON.parse(data);
+    console.table(contacts);
+    return contacts;
+  } catch (error) {
+    console.error(error);
+  }
 }
-console.log(listContacts());
+// console.log(listContacts());
+// listContacts();
 
 async function getContactById(contactId) {
-  const contacts = await readDb();
-  const contact = contacts.find((contact) => contactId == contact.id);
-  console.table(contact);
+  const contacts = await listContacts();
+  const contactById = contacts.find(
+    (contact) => contactId === Number(contact.id)
+  );
+  if (!contactById) {
+    return `Not found this id: ${contactId}`;
+  }
+  console.table(contactById);
+  return contactById;
 }
-console.log(getContactById(1));
+// console.log(getContactById(1));
 
 async function removeContact(contactId) {
-  const contacts = await readDb();
+  const contacts = await listContacts();
+  const updateContacts = contacts.filter(
+    (contact) => Number(contact.id) !== contactId
+  );
+  if (!updateContacts) {
+    return `Not found this id: ${contactId}`;
+  }
+  console.table(updateContacts);
 }
+console.log(removeContact(1));
 
 async function addContact(name, email, phone) {
   // ...твій код
